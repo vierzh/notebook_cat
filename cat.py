bd3c2eef6d35537b527618e2f400a1f651c62a41
vierzh <781323938@qq.com>
2018-07-16 17:47:00 +0800

    Finish more notebooks cating

--- cat.py
+++ cat.py
@@ -1,40 +1,48 @@
 import json
 import sys
 
+#handle argument exceptions
+if len(sys.argv) < 3:
+	raise Exception('参数数量必须大于2')
 
 #accept argument
-notebook_path1 = sys.argv[1]
-notebook_path2 = sys.argv[2]
+notebook_path_lst = sys.argv[1:]
 
-
-#read notebooks
-notebook1 = open(notebook_path1)
-notebook1_str = notebook1.read()
-
-notebook1_json = json.loads(notebook1_str)
-
-cells1 = notebook1_json['cells']
-
-notebook2 = open(notebook_path2)
-notebook2_str = notebook2.read()
-
-notebook2_json = json.loads(notebook2_str)
-
-cells2 = notebook2_json['cells']
-
-
-#delete cells 
-del notebook1_json['cells']
-
-#cat two notebooks
-target_cell = cells1 + cells2
-
+cells_lst = []
 target_notebook = {}
 
-target_notebook['cells'] = target_cell
-target_notebook.update(notebook1_json)
+#read notebook path list
+try:
+	for path in notebook_path_lst:
+		with open(path) as notebook:
+			notebook_str = notebook.read()
+			notebook_json = json.loads(notebook_str)
+			cells = notebook_json['cells']
+
+			del notebook_json['cells']
+
+			cells_lst += cells
+except FileNotFoundError:
+	print('输入文件未找到')
+except json.decoder.JSONDecodeError:
+	print('文件内容格式错误')
+
+target_notebook['cells'] = cells_lst
+
+#add another argument
+try:
+	with open(notebook_path_lst[0]) as ntb:
+		ntb_str = ntb.read()
+		ntb_json = json.loads(ntb_str)
+
+		del ntb_json['cells']
+
+		target_notebook.update(ntb_json)
+except FileNotFoundError:
+	print('输入文件未找到')
+
 
 target_str = json.dumps(target_notebook)
 
-target = open('target_notebook.ipynb', 'w')
-target.write(target_str)
+with open('target_notebook.ipynb', 'w') as target:
+	target.write(target_str)
