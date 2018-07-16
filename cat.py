f73708bad6b8cd45a87ceb73b2d3a78fa32fa7d1
vierzh <781323938@qq.com>
2018-07-16 16:02:17 +0800

    Read

--- cat.py
+++ cat.py
@@ -1,11 +1,21 @@
-
+import json
 
 #read notebooks
-notebook1 = {}
-cells1 = notebook1['cells']
+notebook1 = open('1.ipynb')
+notebook1_str = notebook1.read()
 
-notebook2 = {}
-cells2 = notebook2['cells']
+notebook1_json = json.loads(notebook1_str)
+print(notebook1_json)
+
+cells1 = notebook1_json['cells']
+
+notebook2 = open('2.ipynb')
+notebook2_str = notebook2.read()
+
+notebook2_json = json.loads(notebook2_str)
+print(notebook2_json)
+
+cells2 = notebook2_json['cells']
 
 #cat two notebooks
 target_cell = cells1 + cells2
